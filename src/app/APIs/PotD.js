6fd829414fbafea2b225a4ca448e47ab648129f0
michelangelo17 <michelangelo.markus@gmail.com>
2019-11-05 19:47:02 -0800

    api call is working, will refactor to see if I can make cleaner

--- src/app/APIs/PotD.js
+++ src/app/APIs/PotD.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import { useEffect } from 'react';
+import Axios from 'axios';
 
 const PotD = props => {
+  useEffect(() => {
+    Axios.get(
+      'https://api.nasa.gov/planetary/apod?api_key=imt8zuR2dp53cZeK0DfQxhyJO39CWkDL7gTuZmgK',
+    )
+      .then((res) => {
+        props.setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return null;
-}
+};
 
 export default PotD;
--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import PotD from './APIs/PotD';
 import Title from './title/Title';
@@ -7,8 +7,17 @@ import Description from './description/Description';
 import Date from './date/Date';
 
 function App() {
+  const [data, setData] = useState({date: '', explanation: '', hdurl: '', media_type: '', title: '', url: ''});
+  let nasaDate = data.date;
+  let nasaDescription = data.explanation;
+  let nasaHDimage = data.hdurl;
+  let mediaType = data.media_type;
+  let nasaTitle = data.title;
+  let standardImage = data.url;
+  console.log(nasaDate, nasaDescription, nasaHDimage, mediaType, nasaTitle, standardImage);
   return (
     <div className="App">
+      <PotD setData={setData} />
       <Title />
       <Image />
       <Description />
